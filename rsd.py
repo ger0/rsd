5f2640b8f2a5950c910674ebfcea5812c640b0cc
gero <ger0@github.com>
2021-12-01 15:57:17 +0100

    drobna zmiana

--- rsd.py
+++ rsd.py
@@ -5,7 +5,15 @@ import cv2
 from random import randint as rand
 from matplotlib import pyplot as plt
 
-moments = []
+# liczba znakow 
+TYPES   = 1
+
+# poszukiwane ustawienia
+HUE_MIN     = 190
+HUE_MAX     = 220
+AREA        = 0.5
+MOMENTS     = []
+THRESHOLD   = 32
 
 # dziala?
 def LaplacianOfGaussian(image):
@@ -15,19 +23,19 @@ def LaplacianOfGaussian(image):
     laplac  = cv2.convertScaleAbs(laplac)
     return laplac 
     
-def thresholding(image):
+def thresholding(image, hue_min, hue_max):
     laplac = LaplacianOfGaussian(image)
-    # 190 - 220 hue dla niebieskich znakow 
-    thres = cv2.bitwise_and(laplac,laplac, mask=hueMask(image, 190, 220))
-    thres = cv2.threshold(thres, 32, 255, cv2.THRESH_BINARY)[1]
+    thres = cv2.bitwise_and(laplac,laplac, mask=hueMask(image, hue_min, hue_max))
+    thres = cv2.threshold(thres, THRESHOLD, 255, cv2.THRESH_BINARY)[1]
     return thres
 
 def getText(hum):
-    strink = str('')
+    string = str('')
     for i in hum:
-        strink += str(float(int(i) * 100) / 100) + '\n'
-    return strink
+        string += str(float(int(i) * 100) / 100) + '\n'
+    return string
 
+# przyjmuje wartosc w stopniach 
 def getHue(hue):
     return (int)(hue /2)
 
@@ -43,7 +51,9 @@ def hueMask(img, lower, upper):
 
 def contourImage(filename):
     image   = cv2.imread(filename)
-    thres   = thresholding(image)
+    thres   = [None] * TYPES
+    for i in range(0, TYPES):
+        thres[i]   = thresholding(image, HUE_MIN, HUE_MAX)
 
     #thres = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     #thres = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
@@ -51,12 +61,7 @@ def contourImage(filename):
     #thres = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 10, 15)
 
     # nieco szersze krawedzie 
-    hor = np.array([[0,1,0], [0, 1, 0], [0, 1, 0]], 'uint8')
-    ver = np.array([[0,0,0], [1, 1, 1], [0, 0, 0]], 'uint8')
-
-    #dil = cv2.dilate(thres, hor, iterations=2)
-    #dil = cv2.dilate(dil, ver, iterations=2)
-    dil = cv2.dilate(thres, np.ones((3,3), 'uint8'), iterations=2)
+    dil = cv2.dilate(thres[0], np.ones((3,3), 'uint8'), iterations=2)
     ero = cv2.erode(dil, np.ones((3,3), 'uint8'), iterations=2)
 
     cont, hier = cv2.findContours(ero, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -74,7 +79,6 @@ def contourImage(filename):
         if (bndW * bndH * 0.5 < cv2.contourArea(cont[i])):
         #if (True):
             #approx  = cv2.approxPolyDP(cont[i], 0.01 * cv2.arcLength(cont[i], True), True)
-            '''
             M       = cv2.moments(cont[i])
             huM     = cv2.HuMoments(M)
             for j in range(0,7):
@@ -83,14 +87,17 @@ def contourImage(filename):
                 x = int(M['m10']/M['m00'])
                 y = int(M['m01']/M['m00'])
 
-            moments.append(huM)
-            image = cv2.putText(image, getText(huM),
+            MOMENTS.append(huM)
+            r, g, b = rand(50, 255), rand(50, 255), rand(50, 255)
+            '''image = cv2.putText(image, getText(huM),
                     (x, y), cv2.FONT_HERSHEY_SIMPLEX, 
                        1, (r, g, b), 3, cv2.LINE_AA)
-            '''
-            r, g, b = rand(50, 255), rand(50, 255), rand(50, 255)
+                       '''
             cv2.drawContours(image, cont, i, (r, g, b), 5)
             #cv2.circle(image,(x, y), 2, (0,0,0), 10)
+            #image = thres[0][bndY: bndY + bndH, bndX : bndX + bndW]
+            #image = image[bndY: bndY + bndH, bndX : bndX + bndW]
+            print(huM)
     return image
 
 def main():
