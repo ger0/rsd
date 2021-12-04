8053cbf89576d1fc42bf72420ffb6b68752d664c
ger0 <mrgero155@gmail.com>
2021-12-04 23:24:32 +0100

    maska na niebieski i czerwony kolor

--- rsd.py
+++ rsd.py
@@ -1,18 +1,28 @@
 #!/bin/python
+from enum import Enum
 import sys
 import numpy as np
 from random import randint as rand
 import cv2
 from matplotlib import pyplot as plt
 
+
 # liczba znakow 
-TYPES   = 1
+TYPES   = 2
+
+class Types(Enum):
+    INFO    =   0
+    STOP    =   1
 
 correct_moments = []
 
 # poszukiwane ustawienia
-HUE_MIN     = 190
-HUE_MAX     = 240
+BLUE_MIN    = 195
+BLUE_MAX    = 250
+
+RED_MIN     = 325
+RED_MAX     = 374
+
 HSV_SAT     = 90
 HSV_VAL     = 90
 AREA        = 0.5
@@ -47,64 +57,94 @@ def thresholding(image, hue_min, hue_max):
     #thres = cv2.bitwise_and(laplac, laplac, mask=hueMask(image, hue_min, hue_max))
     #blur    = cv2.GaussianBlur(image, (3,3), 0)
     #blur    = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-    thres   = cv2.bitwise_and(hueMask(image, hue_min, hue_max), hueMask(image, hue_min, hue_max))
+    thres = hueMask(image, hue_min, hue_max)
     thres   = cv2.threshold(thres, THRESHOLD, 255, cv2.THRESH_BINARY)[1]
     return thres
 
-# przyjmuje wartosc w stopniach 
+# przyjmuje wartosc w stopniach (0 - 720)
 def getHue(hue):
-    return (int)(hue / 2)
+    if (hue > 360):
+        return (int)((hue - 360) / 2), True 
+    else:
+        return (int)(hue / 2), False 
 
 # maska na dany kolor
 def hueMask(img, lower, upper, neg=False):
     image   = cv2.GaussianBlur(img, (3,3), 0) 
     hsv     = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    l_col   = np.array([getHue(lower), HSV_SAT, HSV_VAL])
-    u_col   = np.array([getHue(upper),255,255])
-    mask    = cv2.inRange(hsv, l_col, u_col)
-    return cv2.bitwise_and(mask, mask)
+    l, _ = getHue(lower)
+    u, isOverlap = getHue(upper)
+
+    if (isOverlap):
+        l_col = np.array([l, HSV_SAT, HSV_VAL])
+        u_col = np.array([180, 255, 255])
+
+        l1_col  = np.array([0, HSV_SAT, HSV_VAL])
+        u1_col  = np.array([u, 255, 255]) 
+
+        mask    = cv2.inRange(hsv, l_col, u_col)
+        mask1   = cv2.inRange(hsv, l1_col, u1_col)
+        return cv2.bitwise_or(mask, mask1)
+    else:
+        l_col = np.array([l, HSV_SAT, HSV_VAL])
+        u_col = np.array([u, 255, 255])
+
+        mask    = cv2.inRange(hsv, l_col, u_col)
+        return cv2.bitwise_and(mask, mask)
 
 def contourImage(filename, moments):
     image   = cv2.imread(filename)
     norm    = normalizeHist(image)
-    calcAverages(norm)
+    calcAverages(image)
     thres   = [None] * TYPES
+    dil     = [None] * TYPES
+    ero     = [None] * TYPES
+    # dla kazdego typu koloru
     for i in range(0, TYPES):
-        thres[i]   = thresholding(norm, HUE_MIN, HUE_MAX)
+        if (i == 0):
+            thres[i]    = thresholding(norm, BLUE_MIN, BLUE_MAX)
+        elif (i == 1):
+            # overlap : 420 = 360 + 60
+            thres[i]    = thresholding(norm, RED_MIN, RED_MAX)
 
-    dil = cv2.dilate(thres[0], np.ones((3,3), 'uint8'), iterations=2)
-    ero = cv2.erode(dil, np.ones((3,3), 'uint8'), iterations=2)
+        dil[i]  = cv2.dilate(thres[i], np.ones((3,3), 'uint8'), iterations=2)
+        ero[i]  = cv2.erode(dil[i], np.ones((3,3), 'uint8'), iterations=2)
 
-    cont, hier = cv2.findContours(ero, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    for i in range(0, len(cont)):
-        #areas.append #
-        bndX, bndY, bndW, bndH = cv2.boundingRect(cont[i])
+        cont, hier = cv2.findContours(ero[i], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        for iter_cont in range(0, len(cont)):
+            #areas.append #
+            bndX, bndY, bndW, bndH = cv2.boundingRect(cont[iter_cont])
 
-        if (bndW * bndH * AREA < cv2.contourArea(cont[i])):
-        #if (True):
-            #approx  = cv2.approxPolyDP(cont[i], 0.01 * cv2.arcLength(cont[i], True), True)
-            M       = cv2.moments(cont[i])
-            huM     = cv2.HuMoments(M)
-            for j in range(0,7):
-                huM[j] = -1 * np.copysign(1.0, huM[j]) * np.log10(abs(huM[j]))
-            if M['m00'] != 0.0:
-                x = int(M['m10']/M['m00'])
-                y = int(M['m01']/M['m00'])
+            if (bndW * bndH * AREA < cv2.contourArea(cont[iter_cont])):
+            #if (True):
+                #approx  = cv2.approxPolyDP(cont[i], 0.01 * cv2.arcLength(cont[i], True), True)
+                M       = cv2.moments(cont[iter_cont])
+                huM     = cv2.HuMoments(M)
+                for k in range(0,7):
+                    huM[k] = -1 * np.copysign(1.0, huM[k]) * np.log10(abs(huM[k]))
+                if M['m00'] != 0.0:
+                    x = int(M['m10']/M['m00'])
+                    y = int(M['m01']/M['m00'])
 
-            moments.append(huM)
-            r, g, b = rand(50, 255), rand(50, 255), rand(50, 255)
-            image = cv2.putText(image, str(i),
-                    (x, y), cv2.FONT_HERSHEY_SIMPLEX, 
-                       1, (r, g, b), 3, cv2.LINE_AA)
-                       
-            cv2.drawContours(image, cont, i, (r, g, b), 5)
-            cv2.circle(image,(x, y), 2, (255,255,255), 5)
-            #image = thres[0][bndY: bndY + bndH, bndX : bndX + bndW]
-            #image = image[bndY: bndY + bndH, bndX : bndX + bndW]
+                moments.append(huM)
+                r, g, b = 0, 0, 0
+                if (i == 0):
+                    r, g, b = 50, 50, 255
+                elif (i == 1):
+                    r, g, b = 255, 50, 50
 
-            #print(i)
-            #print(huM)
+                # draw
+                image = cv2.putText(image, str(i),
+                        (x, y), cv2.FONT_HERSHEY_SIMPLEX, 
+                           1, (b, g, r), 3, cv2.LINE_AA)
+                cv2.drawContours(image, cont, iter_cont, (b, g, r), 5)
+                cv2.circle(image,(x, y), 2, (255,255,255), 5)
+                #image = thres[0][bndY: bndY + bndH, bndX : bndX + bndW]
+                #image = image[bndY: bndY + bndH, bndX : bndX + bndW]
+
+                #print(i)
+                #print(huM)
     return image 
 
 def main():
@@ -116,12 +156,7 @@ def main():
         #contourImage('znaki/rondo.jpg', correct_moments) 
         print('-------------------------------')
 
-        print(sys.argv[1:])
         fileList = sys.argv[1:]
-
-        image = contourImage(fileList[0], MOMENTS)
-        if image is None:
-            sys.exit("Could not read the image.")
 
         '''
         cv2.imshow("floating", image)
@@ -133,7 +168,10 @@ def main():
         fig = plt.figure(figsize=(8,8))
 
         for i in range(0, len(fileList)):
+            print(fileList[i])
             image = contourImage(fileList[i], MOMENTS)
+            if image is None:
+                sys.exit("Could not read the image.")
             #fig.add_subplot(3, 2, i + 1)
             fig.add_subplot(1,1,1)
             plt.axis('off')
