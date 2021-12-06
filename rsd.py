d4ac00915412ef06aefa9219bb83d07591d1c5a0
Николай Чеботаревич <kdiebro13012002@gmail.com>
2021-12-06 01:45:37 +0100

    Push test

--- rsd.py
+++ rsd.py
@@ -11,7 +11,7 @@ candidates      = []
 
 AREA        = 0.5
 isSetup     = True
-#push test #5
+#push test #6
 def contourImage(filename, typ = None):
     image, norm = pre.loadNorm(filename, isSetup)
     thres, dil, ero  = {}, {}, {}
